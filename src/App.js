f46fa3e77b5ad66426ec4851eb1c2f552d414244
el1syum <kilmol606@gmail.com>
2023-05-17 21:58:56 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+
+    const addCash = (cash) => {
+        dispatch({type:'ADD', payload: cash})
+    }
+
+    const wasteCash = (cash) => {
+        dispatch({type:'GET_CASH', payload: cash})
+    }
+
+    return (
+        <div>
+            <div>{cash}</div>
+            <div style={{display: 'flex'}}>
+                <button onClick={() => addCash(Number(prompt()))}>Add</button>
+                <button onClick={() => wasteCash(Number(prompt()))}>Waste</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
